08b6c7bb7b0b6a175827236dc35a4110853393d3
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-09 16:43:33 +0300

    Finished styling the newbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,36 +1,42 @@
 import React from 'react';
+import evergreen from './logo/evergreen.jpg';
 import './Navbar.css';
 
 export default function Navbar () {
     return (
-    <div className="navbar">
+    <div>
     <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-style">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Evergreen Farms Limited</a>
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src={evergreen}
+       alt="Logo" width="200" height="150" class="d-inline-block align-text-top"/>
+    </a>
+  </div>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link text-light" href="#">Link</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item text-light" href="#">Action</a></li>
+            <li><a className="dropdown-item text-light" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider text-light"/></li>
+            <li><a className="dropdown-item text-light" href="#">Something else here</a></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <a className="nav-link disabled text-light">Disabled</a>
         </li>
       </ul>
        <form className="d-flex">

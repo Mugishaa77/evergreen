2351ce3f503b63aca15d02e78d6d71fee69c7937
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-09 12:57:46 +0300

    installed bootstrap and jquery

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootsrap.css';
+import 'jquery';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

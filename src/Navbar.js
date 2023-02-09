858726aa1471e0a62d9b0b3f13d0ead7bbf81c68
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-09 14:44:05 +0300

    Added a component for the main content

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,8 @@ import './Navbar.css';
 
 export default function Navbar () {
     return (
-    <div classNameName="navbar">
-         <nav classNameName="navbar navbar-expand-lg bg-body-tertiary navbar-style">
+    <div className="navbar">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-style">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Evergreen Farms Limited</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

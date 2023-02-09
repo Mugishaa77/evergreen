3c53092a10bbc1cfd3cfea8b5367c7183309ca01
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-09 14:13:22 +0300

    styled the navbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootsrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery';
 import reportWebVitals from './reportWebVitals';
 
